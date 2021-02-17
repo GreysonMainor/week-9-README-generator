6d2024b2e20c89ddc6161f74221137cdcb2711da
Greyson Mainor <gbmainor98@outlook.com>
2021-02-17 17:37:37 -0500

    added the questions I need to ask my user.

--- Develop/index.js
+++ Develop/index.js
@@ -4,14 +4,58 @@ const fs = require("fs");
 const generateMarkdown = require("./utils/generateMarkdown")
 // TODO: Create an array of questions for user input
 const questions = [
-
+    {
+        type: "input",
+        message: "What is your GitHub username?",
+        name: "github"
+    },
+    {
+        type: "input",
+        message: "What is your email address?",
+        name: "email"
+    },
+    {
+        type: "input",
+        message: "What is your project title?",
+        name: "title"
+    },
+    {
+        type: "input",
+        message: "Add a description for your project.",
+        name: "description"
+    },
+    {
+        type: "input",
+        message: "What kind of license should your project have?",
+        name: "license"
+    },
+    {
+        type: "input",
+        message: "What command should be run to install dependencies?",
+        name: "install"
+    },
+    {
+        type: "input",
+        message: "What command should be run to run tests?",
+        name: "tests"
+    },
+    {
+        type: "input",
+        message: "What does your user need to know to about using the repo?",
+        name: "using"
+    },
+    {
+        type: "input",
+        message: "What does the user need to know about contributing to this repo?",
+        name: "contribute"
+    },
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) { }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() { }
 
 // Function call to initialize app
 init();
